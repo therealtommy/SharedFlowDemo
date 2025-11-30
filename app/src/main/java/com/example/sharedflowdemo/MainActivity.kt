f126008300e0628aa681e4ba86e9a96a3aadb507
therealtommy <sokolovai1887@ya.ru>
2025-11-30 17:08:37 +0700

    Add Print fuction to collent data in LogCat

--- app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
+++ app/src/main/java/com/example/sharedflowdemo/MainActivity.kt
@@ -7,16 +7,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.sharedflowdemo.ui.theme.SharedFlowDemoTheme
 import com.example.sharedflowdemo.DemoViewModel
 import kotlinx.coroutines.flow.SharedFlow
@@ -36,13 +40,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ScreenSetup(modifier: Modifier = Modifier,    viewModel: DemoViewModel = viewModel()) {
+fun ScreenSetup(
+    modifier: Modifier = Modifier,
+    viewModel: DemoViewModel = viewModel()
+) {
     MainScreen(modifier,viewModel.sharedFlow)
 }
 @Composable
 fun MainScreen(modifier: Modifier = Modifier,sharedFlow: SharedFlow<Int>) {
 
     val messages = remember { mutableStateListOf<Int>()}
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    LaunchedEffect(key1 = Unit) {
+        sharedFlow.collect {
+            println("Collecting $it")
+            messages.add(it)
+        }
+    }
 
     LazyColumn(modifier = modifier) {
         items(messages) {
